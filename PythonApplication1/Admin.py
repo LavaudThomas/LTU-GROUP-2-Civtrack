4f22254bc045537237a76763c92a77b8b3a34fc9
LavaudThomas <thomaslavaud391@gmail.com>
2026-04-16 15:42:27 -0400

    Got started with User and Admin classes
    did user and admin may have to add more to them later

---
+++ PythonApplication1/Admin.py
@@ -0,0 +1,25 @@
+﻿from User import User
+class Admin(User):
+    def __init__(self, userID, username, email, password, adminID):
+        super().__init__(userID, username, email, password, isAdmin=True)
+        self._adminID = adminID
+        self._pendingReports = []   # report[0..*] → list
+
+    # Public methods
+    def reviewReport(self, report):
+        print(f"Reviewing report: {report.title}")
+
+    def approveReport(self, report):
+        report.status = "Approved"
+        print(f"Report '{report.title}' approved")
+
+    def rejectReport(self, report):
+        report.status = "Rejected"
+        print(f"Report '{report.title}' rejected")
+
+    def updateReportStatus(self, report, status):
+        report.status = status
+        print(f"Report '{report.title}' updated to {status}")
+
+    def deleteComment(self, comment):
+        print("Comment deleted")
---
+++ PythonApplication1/User.py
@@ -0,0 +1,31 @@
+class User:
+    def __init__(self, userID, username, email, password, isAdmin=False, isGuest=False):
+        self._userID = userID
+        self._username = username
+        self._email = email
+        self._password = password
+        self._isAdmin = isAdmin
+        self._isGuest = isGuest
+        self._loggedIn = False
+
+    # Public methods
+    def login(self, password):
+        if password == self._password:
+            self._loggedIn = True
+            print("Login successful")
+        else:
+            print("Incorrect password")
+
+    def logout(self):
+        self._loggedIn = False
+        print("Logged out")
+
+    def updatePassword(self, newPassword):
+        self._password = newPassword
+        print("Password updated")
+
+    def submitReport(self, report):
+        print(f"{self._username} submitted a report: {report.title}")
+
+
+
